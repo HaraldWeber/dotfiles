641443c831f8d66ed653b3fd5459343fd2e20b91
Harald Weber <harald.h.weber@gmail.com>
2013-08-27 22:23:01 +0200

    remove the directories before bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-rsync --exclude "*/.git" --exclude ".git" --exclude ".directory" --exclude ".gitignore" --exclude ".gitmodules" --exclude "bootstrap.sh" -av . ~/
+
+rm -r ~/.bash
+rm -r ~/.screen
+rm -r ~/.vim
+rsync --exclude "*/.git" --exclude ".git" --exclude ".directory" --exclude ".gitignore" --exclude ".gitmodules" --exclude "bootstrap.sh" -av .  ~/
