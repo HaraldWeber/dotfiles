9ae68ec19c64b985f91f101de61f3be65d99fc28
Harald Weber <harald.h.weber@gmail.com>
2013-12-05 10:34:42 +0100

    source the new files after a bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,4 @@ rm -r ~/.bash
 rm -r ~/.screen
 rm -r ~/.vim
 rsync --exclude "*/.git" --exclude ".git" --exclude ".directory" --exclude ".gitignore" --exclude ".gitmodules" --exclude "bootstrap.sh" -av .  ~/
+source ~/.bashrc
