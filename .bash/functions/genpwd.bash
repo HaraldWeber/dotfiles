340bf0c7786936c037a0515ba72a05c0d5713b5b
Harald Weber <harald.h.weber@gmail.com>
2014-03-08 08:22:21 +0100

    function genpwd has now 2 parameters (PWD_LEN and COUNT)

--- .bash/functions/genpwd.bash
+++ .bash/functions/genpwd.bash
@@ -1,7 +1,17 @@
 #!/bin/bash
 function genpwd() {
-for i in {1..10}
+PWD_LEN=$1
+COUNT=$2
+if [[ -z "${PWD_LEN}" ]]; then
+    PWD_LEN=8
+fi
+if [[ -z "${COUNT}" ]]; then
+    COUNT=10
+fi
+for i in $(eval echo "{1..$COUNT}")
 do
-    head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 16; echo
+    head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c ${PWD_LEN}; echo
 done
+# echo "Usage: genpwd <Password Length> <Count>" > /dev/stderr
+# echo "Standard is genpwd 8 10" >&2
 }
