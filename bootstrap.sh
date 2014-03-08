1a4df44bb998097bda91c761431021bd7429fd01
Harald Weber <harald.h.weber@gmail.com>
2014-03-08 08:41:31 +0100

    exclude update.sh and README from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 rm -r ~/.bash
 rm -r ~/.screen
 rm -r ~/.vim
-rsync --exclude "*/.git" --exclude ".git" --exclude ".directory" --exclude ".gitignore" --exclude ".gitmodules" --exclude "bootstrap.sh" -av .  ~/
+rsync --exclude "*/.git" --exclude ".git" --exclude ".directory" --exclude ".gitignore" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "update.sh" --exclude "README.md" -av .  ~/
 source ~/.bashrc
