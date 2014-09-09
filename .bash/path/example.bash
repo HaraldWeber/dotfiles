dfa0166320dfde047d9bbfb3c23026dbab615d09
Harald Weber <harald.h.weber@gmail.com>
2014-09-09 20:07:35 +0200

    PATH exports can be written into seperate files

---
+++ .bash/path/example.bash
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Include files with extra path definitions
+# These definitions should be not general definitions like for cygwin stuff on windows machines
+#
+# PATH=$PATH:<some_path>
