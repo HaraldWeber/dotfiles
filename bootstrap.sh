3b70af0324a04ede42732c3b965c6a7452e6603e
HaraldWeber <harald.h.weber@gmail.com>
2014-09-18 10:59:55 +0200

    do not delete .bash and .vim
    allow custom vim plugins and scripts in .bash

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm -r ~/.bash
 rm -r ~/.screen
-rm -r ~/.vim
 rsync --exclude "*/.git" --exclude ".git" --exclude ".directory" --exclude ".gitignore" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "update.sh" --exclude "README.md" -av .  ~/
 source ~/.bashrc
