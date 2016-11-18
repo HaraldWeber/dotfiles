51d660b4bcef565f311f9a1fd96aecfcefde06fe
Harald Weber <harald.h.weber@gmail.com>
2016-11-18 07:29:20 +0100

    Added custom directory for general exports

---
+++ .bash/exports/example.bash
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Include files with extra export definitions
+# These definitions should be not general definitions like for cygwin stuff on windows machines
+#
+# export VARIABLE=value
