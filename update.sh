509e892025c15c30ac053fa394e3dea44a1cf3e6
Harald Weber <harald.h.weber@gmail.com>
2020-02-21 12:10:47 +0100

    allways update vim plugins from latest master

--- update.sh
+++ update.sh
@@ -4,7 +4,16 @@
 
 git pull
 
-git submodule update --init --recursive
+# vim plugins
+GITHUB="https://github.com"
+VIM_PLUGINS="${GITHUB}/scrooloose/nerdtree ${GITHUB}/altercation/vim-colors-solarized"
+
+for PLUGIN in ${VIM_PLUGINS}
+do
+    PLUGIN_NAME=$(basename ${PLUGIN})
+    LOCAL_REPO=".vim/bundle/${PLUGIN_NAME}"
+    git clone "${PLUGIN}" "${LOCAL_REPO}" 2> /dev/null || git -C "${LOCAL_REPO}" pull
+done
 
 ./bootstrap.sh
 
