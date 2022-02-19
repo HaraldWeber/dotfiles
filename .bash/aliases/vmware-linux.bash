6018e33fda05a8393a4781429ac20c1121ad11ff
Harald Weber <harald.h.weber@gmail.com>
2022-02-19 08:28:19 +0100

    start and connect to linux vm

---
+++ .bash/aliases/vmware-linux.bash
@@ -0,0 +1,2 @@
+#alias linux='{ ssh -o ConnectTimeout=1 linux; } || { echo "Linux not running. Starting..."; /cygdrive/c/Program\ Files\ \(x86\)/VMware/VMware\ Workstation/vmrun.exe -T ws start "V:\VMware\Linux\Linux.vmx";  ssh linux; }'
+alias linux='ssh -o ConnectTimeout=1 linux; (( $? == 255 )) && { /cygdrive/c/Program\ Files\ \(x86\)/VMware/VMware\ Workstation/vmrun.exe -T ws start "V:\VMware\Linux\Linux.vmx";  ssh linux; }'
