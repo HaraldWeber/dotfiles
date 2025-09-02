cba477a67b9e35b71d8fe778d1517a01902ba7e3
Harald Weber <harald.h.weber@gmail.com>
2025-09-02 15:46:56 +0200

    moving vm to another directory

--- .bash/aliases/vmware-linux.bash
+++ .bash/aliases/vmware-linux.bash
@@ -1,2 +1,2 @@
 #alias linux='{ ssh -o ConnectTimeout=1 linux; } || { echo "Linux not running. Starting..."; /cygdrive/c/Program\ Files\ \(x86\)/VMware/VMware\ Workstation/vmrun.exe -T ws start "V:\VMware\Linux\Linux.vmx";  ssh linux; }'
-alias linux='ssh -Y -o ConnectTimeout=1 linux; (( $? == 255 )) && { /cygdrive/c/Program\ Files\ \(x86\)/VMware/VMware\ Workstation/vmrun.exe -T ws start "D:\VMware\Linux\Linux.vmx";  ssh linux; }'
+alias linux='ssh -Y -o ConnectTimeout=1 linux; (( $? == 255 )) && { /cygdrive/c/Program\ Files\ \(x86\)/VMware/VMware\ Workstation/vmrun.exe -T ws start "C:\Users\hari\VMware\Linux\Linux.vmx";  ssh linux; }'
